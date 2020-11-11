1da2078a20432eee96ebce3e27ab0448d423a375
Patricia Zudio de Lima <pzdelima@gmail.com>
2020-11-11 17:19:11 -0300

    primeiro commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Projeto do Curso Mlops Deploy de Modelos da Alura',
+    author='Patrícia Zudio',
+    license='',
+)
